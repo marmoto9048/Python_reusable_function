15331569bffa3d7a152952532136cafa655fc5ba
noober_cypok <34801729+marmoto9048@users.noreply.github.com>
2021-07-05 14:28:12 +0900

    Update txt读取_存储.py

--- txt读取_存储.py
+++ txt读取_存储.py
@@ -13,6 +13,8 @@ def read_file(file_path):
         line = f.readline()
     f.close()
     return sentence_lst
+
+
 def save(file,data):
     filename = open(file,'a+')
     for value in data:
@@ -25,3 +27,8 @@ def n_line():
     f.writelines(lists) #1 是不换行的写入，可用以下方法在写入时换行。
     for line in lists:      #2  换行
         f.write(line+'\n')
+
+def write_in_file():
+    f = open('test.txt', 'w') # 若是'wb'就表示写二进制文件
+    f.write('Hello, world!') #写入一行
+    f.close()
