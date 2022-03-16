c136ee1e2bdf035edf91cd834d685b685f976b24
noober_cypok <34801729+marmoto9048@users.noreply.github.com>
2022-03-16 14:16:09 +0900

    Update pandas 存入csv文件并设置分隔符.py

--- pandas 存入csv文件并设置分隔符.py
+++ pandas 存入csv文件并设置分隔符.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 df = pd.read_excel('./meta_info.xlsx')
 df.to_csv('./meta_info.csv',sep=';')
-
-
-
+ 
+#设置列名
+city = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
+city.to_csv('city.csv')
