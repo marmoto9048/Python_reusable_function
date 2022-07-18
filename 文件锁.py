2812c6c9fb044b0d593483b6b146eef96c6315a1
noober_cypok <34801729+marmoto9048@users.noreply.github.com>
2022-07-19 08:33:14 +0900

    Update 文件锁.py

--- 文件锁.py
+++ 文件锁.py
@@ -10,7 +10,7 @@
     with open('sql_daily_cmd.csv', 'a+', encoding='utf-8') as f:
         fcntl.flock(f.fileno(), fcntl.LOCK_EX)  # 加锁，with块外自动释放锁
         f.truncate(0)
-        cmd_SQL2 = ''' "SELECT A.製造NO AS ロットNo, A.品目KEY, A.品目CD, B.子品目KEY, B.子品目CD, A.指示数 AS 数量, B.実払出数 AS 所要数,  1 AS 前加工有無 FROM D_HR払出計画データ AS A INNER JOIN D_HR払出構成データ AS B ON A.製造NO = B.製造NO WHERE ((A.払出完了日付 = ''' +sql_data_day1 +''' AND A.払出完了時間 > ''' +sql_data_hour1+''' ) or (A.払出完了日付 >= '''+ str(int(sql_data_day1)+1)+''')) AND A.払出完了日付 <> 20991231 AND A.品目CD LIKE '%$' AND A.指示数 <> 0 ORDER BY A.払出完了日付 DESC, A.払出完了時間 DESC  " -o sql_daily_cmd.csv -h-1 -W -s ";" '''
+        cmd_SQL2 = ''' "select number from table1" -o sql_daily_cmd.csv -h-1 -W -s ";" '''
         print('cmd_SQL2',cmd_SQL2)
         cmd2=sql_server+cmd_SQL2
         # print('cmd2',cmd2)
