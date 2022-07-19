21bb64cc1e519e03effc8321cf93ca33bed6359f
noober_cypok <34801729+marmoto9048@users.noreply.github.com>
2022-07-19 09:19:13 +0900

    Update 1-evaluating-blue-meteor.py

--- evaluation/1-evaluating-blue-meteor.py
+++ evaluation/1-evaluating-blue-meteor.py
@@ -1,10 +1,10 @@
 from Read_Save import save
 from nlgeval import compute_metrics
 
-metrics_dict = compute_metrics(hypothesis='2-PQG/3-predict-100.txt',references=['2-PQG/3-reference-100.txt'])# #
+metrics_dict = compute_metrics(hypothesis='predict-100.txt',references=['reference.txt'])# #
 # input_file_path = "test-100.txt"
 print('metrics_dict',metrics_dict)
-output_file_path = "2-PQG/results.txt"
+output_file_path = "results.txt"
 save(output_file_path, metrics_dict)
 
 
