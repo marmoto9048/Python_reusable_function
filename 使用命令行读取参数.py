9dd56d320cb8e2bc730aa4ed4bdf8247e0e8faa8
noober_cypok <34801729+marmoto9048@users.noreply.github.com>
2022-07-20 15:55:19 +0900

    Update 使用命令行读取参数.py

--- 使用命令行读取参数.py
+++ 使用命令行读取参数.py
@@ -1,5 +1,5 @@
-from gec_model import GecBERTModel
-from predict_sent2 import grammer_use
+from gec_model import Model
+from predict_sent import grammer_use
 from grammer_score import final_score
 import argparse
 def grammar_checker(input_sent):
@@ -13,8 +13,6 @@ def grammar_checker(input_sent):
 if __name__=='__main__':
     import sys
     input_sentence=str(sys.argv[1])
-    # print('得到的输入句子：',input_sentence)
     output_sent,final_score = grammar_checker(input_sentence)
-    # print('sentence score:', output_sent,final_score)
     print( output_sent,final_score)
 
